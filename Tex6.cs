14fe82b0c5ef5737a99e7a30dada779653b5f043
Moonyll <s.pouwel@laposte.net>
2018-12-18 09:47:54 +0100

    Tex6

---
+++ Tex6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        enum week
+        {
+            lundi,
+            mardi,
+            mercredi,
+            jeudi,
+            vendredi,
+            saemdi,
+            dimanche
+        }
+        static void Main(string[] args)
+        {
+        // foreach (int i in Enum.GetValues(typeof(week)))
+        foreach (string i in Enum.GetNames(typeof(week)))
+        Console.WriteLine(i);
+        }
+    }
+}
